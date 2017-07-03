6df722b5b44711e796dc828c9590da80190f2fae
alexander <san4ezy@gmail.com>
2017-07-03 18:09:38 +0300

    pypi preparing

--- setup.py
+++ setup.py
@@ -5,11 +5,17 @@ setup(
     name="FastCoinExchange",
     version=__import__('fastcoinexchange-python').__version__,
     description=open(os.path.join(os.path.dirname(__file__), "DESCRIPTION")).read(),
+    license="The MIT License (MIT)",
     keywords="coin, exchange",
 
+    author="Alexander Yudkin",
+    author_email="alexander@yudkin.com.ua",
+
+    maintainer="Alexander Yudkin",
+    maintainer_email="alexander@yudkin.com.ua",
 
     url="https://fastcoinexchange.com/",
-    packages=find_packages(exclude=["tests.*", "tests"]),
+    # packages=find_packages(exclude=["tests.*", "tests"]),
     classifiers=[
         'Intended Audience :: Developers',
         'Environment :: Web Environment',
