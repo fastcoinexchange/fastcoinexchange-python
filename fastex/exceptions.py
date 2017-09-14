c648deb881dc399066228e56af7aad97aad5906b
alexander <san4ezy@gmail.com>
2017-09-14 18:13:44 +0300

    works with server

--- fastex/exceptions.py
+++ fastex/exceptions.py
@@ -1,23 +1,33 @@
-class APIError(Exception):
+class FastexAPIError(Exception):
     def __init__(self, code, msg):
         self.code = code
         self.msg = msg
 
     def __str__(self):
         return 'FastCoinExchange APIError "%s" (code: %s)' % (self.msg, self.code)
+#
+#
+# class UnknownRequestMethod(Exception):
+#     def __init__(self, method):
+#         self.method = method
+#
+#     def __str__(self):
+#         return 'Unknown request method: %s' % self.method
+#
+#
+# class UnknownResponseKey(Exception):
+#     def __init__(self, key):
+#         self.key = key
+#
+#     def __str__(self):
+#         return 'Unknown response key: %s' % self.key
 
 
-class UnknownRequestMethod(Exception):
-    def __init__(self, method):
-        self.method = method
-
+class FastexInvalidDataReceived(Exception):
     def __str__(self):
-        return 'Unknown request method: %s' % self.method
+        return 'Invalid data received'
 
 
-class UnknownResponseKey(Exception):
-    def __init__(self, key):
-        self.key = key
-
+class FastexBadDataDecoded(Exception):
     def __str__(self):
-        return 'Unknown response key: %s' % self.key
+        return 'Bad data decoded'
